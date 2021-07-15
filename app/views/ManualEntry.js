3334b25e8b11abc4c70fe1c1a99473aec677e52f
Rohan Minocha <rohan.minocha2002@gmail.com>
2021-07-15 14:36:08 -0400

    Create ManualEntry.js

---
+++ app/views/ManualEntry.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+import {View, Text, SafeAreaView} from 'react-native';
+
+const ManualEntry = ({navigation, route}) => {
+    return(
+        <SafeAreaView>
+            <Text>ManualEntry</Text>
+        </SafeAreaView>
+    );
+}
+
+
+export default ManualEntry
