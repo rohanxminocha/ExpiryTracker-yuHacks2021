16aaa5ec96818f14a198d7f05145a0297a22361f
Rohan Minocha <rohan.minocha2002@gmail.com>
2021-07-15 14:37:31 -0400

    Add files via upload

---
+++ App/views/BarcodeEntry.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+import {View, Text, SafeAreaView} from 'react-native';
+
+const BarcodeEntry = ({navigation, route}) => {
+    return(
+        <SafeAreaView>
+            <Text>BarcodeEntry</Text>
+        </SafeAreaView>
+    );
+}
+
+
+export default BarcodeEntry
---
+++ App/views/Fridge.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+import {View, Text, SafeAreaView} from 'react-native';
+
+const Fridge = ({navigation, route}) => {
+    return(
+        <SafeAreaView>
+            <Text>Fridge</Text>
+        </SafeAreaView>
+    );
+}
+
+
+export default Fridge
